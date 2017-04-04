df3a1f20fb67ad9a8f229de1f7f8bd67483cb418
Matt Gershowitz <mattgershowitz1@gmail.com>
2017-04-04 15:46:00 -0400

    hw init

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -6,39 +6,34 @@ class App extends Component {
   constructor(props){
     super(props)
 
-    this.setTime = this.setTime.bind(this);
     this.runTimer = this.runTimer.bind(this);
     this.stopTimer = this.stopTimer.bind(this)
+    this.resetTimer = this.resetTimer.bind(this)
     this.state = {
       timer: 0
     }
   }
 
-  setTime(e){
-    e.preventDefault()
-    let timer = e.target.timer.value;
-    console.log(e.target.timer.value);
-    this.setState({
-      timer: timer
-    })
-  }
-
   runTimer(){
     clearInterval(this.run)
     let time = this.state.timer
-    let that = this
-    this.run = setInterval(function(){
-      if(time > 0){
-        that.setState({
-          timer: time -= 1
-        })
-      }
-    }, 1000)
-    }
+    this.run = setInterval(()=>{
+      this.setState({
+        timer: time += 1
+      })
+    }, 10)
+  }
 
 
     stopTimer(){
       clearInterval(this.run)
+    }
+
+    resetTimer(){
+      clearInterval(this.run)
+      this.setState({
+        timer: 0
+      })
     }
 
 
@@ -46,12 +41,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>{this.state.timer}</h1>
-        <form onSubmit={this.setTime}>
-          <input name="timer" type="text"/>
-          <button>set timer</button>
-        </form>
         <button onClick={this.runTimer}>run</button>
         <button onClick={this.stopTimer}>stop</button>
+        <button onClick={this.resetTimer}>reset</button>
       </div>
     );
   }
