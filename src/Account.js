99f592b116b92391102a5e3d5ccc0a91318e0763
Trevor Preston <trevorpreston4@gmail.com>
2017-04-11 11:49:44 -0400

    add solution

---
+++ src/Account.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+export default class Account extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      balance: 0
+    }
+
+    this._handleDepositClick = this._handleDepositClick.bind(this)
+    this._handleWithdrawalClick = this._handleWithdrawalClick.bind(this)
+  }
+
+  _handleDepositClick(e) {
+    e.preventDefault()
+    let amount = +this.refs.amount.value
+    let newBalance = this.state.balance + amount;
+    this.setState({
+      balance: newBalance
+    })
+    this.refs.amount.value = '';
+  }
+
+  _handleWithdrawalClick(e) {
+    e.preventDefault()
+    let amount = +this.refs.amount.value;
+    let newBalance = this.state.balance - amount;
+    if (newBalance < 0) {
+      newBalance = 0
+    }
+    this.setState({
+      balance: newBalance
+    })
+    this.refs.amount.value = '';
+  }
+
+  render() {
+    let balanceClass = 'balance';
+    if (this.state.balance === 0) {
+      balanceClass += ' zero';
+    }
+
+    return (
+      <div className="account">
+        <h2>{this.props.name}</h2>
+        <div className={balanceClass}>${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref="amount" />
+        <input type="button" value="Deposit" onClick={this._handleDepositClick} />
+        <input type="button" value="Withdraw" onClick={this._handleWithdrawalClick} />
+      </div>
+    )
+  }
+}
